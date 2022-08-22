c5efa312d9436efebc715023a0f4559007cab360
Artem Meshcherov <spacenail@gmail.com>
2022-08-22 17:40:06 +0300

    lesson1

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,6 +10,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	int clicks = 0;
 	
 	@Override
 	public void create () {
@@ -18,8 +21,20 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 1, 1, 1);
+
+		float x = Gdx.input.getX()- img.getWidth()/2f;
+		float y = Gdx.graphics.getHeight() - Gdx.input.getY()- img.getHeight()/2f;
+
 		batch.begin();
-		batch.draw(img, 200, 150);
+
+		if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)){
+			clicks +=1;
+		}
+		String text = "Вы нажали левую кнопку мыши " + clicks + " раз!";
+		Gdx.graphics.setTitle(text);
+
+		batch.draw(img, x, y);
+
 		batch.end();
 	}
 	
