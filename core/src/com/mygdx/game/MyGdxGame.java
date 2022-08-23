abf7cdd1ae726c349d0bf6f351caf6f62b3f3d83
Artem Meshcherov <spacenail@gmail.com>
2022-08-23 21:27:49 +0300

    add simple animation

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,45 +2,32 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	int clicks = 0;
-	
+	MyAnimation myAnimation;
+	float time;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("gift.png");
+		myAnimation = new MyAnimation("sprites.png");
 	}
 
 	@Override
 	public void render () {
 		ScreenUtils.clear(1, 1, 1, 1);
-
-		float x = Gdx.input.getX()- img.getWidth()/2f;
-		float y = Gdx.graphics.getHeight() - Gdx.input.getY()- img.getHeight()/2f;
-
 		batch.begin();
-
-		if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)){
-			clicks +=1;
-		}
-		String text = "Вы нажали левую кнопку мыши " + clicks + " раз!";
-		Gdx.graphics.setTitle(text);
-
-		batch.draw(img, x, y);
-
+		time += Gdx.graphics.getDeltaTime();
+		batch.draw(myAnimation.getKeyFrame(time), 0 ,0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		myAnimation.dispose();
 	}
 }
